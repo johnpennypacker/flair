7380cda443b3974cccebdfbe7c1c343be5fd3dff
John Pennypacker <jpennypacker@uri.edu>
2022-05-22 06:50:29 -0400

    allows for equal-height cards; experimenting with using a pattern instead

--- flair.php
+++ flair.php
@@ -14,12 +14,15 @@ if ( !defined('ABSPATH') )
 	die('-1');
 
 /**
- * Add the main flair script
+ * Add the main flair script and stylesheet
  */
-function flair_script() {
+function flair_enqueus() {
 	wp_enqueue_script( 'flair', plugins_url( 'flair.js', __FILE__ ), array() );
+
+	wp_enqueue_style( 'flair', plugins_url( 'flair.css', __FILE__ ), array() );
+
 }
-add_action( 'wp_enqueue_scripts', 'flair_script' );
+add_action( 'wp_enqueue_scripts', 'flair_enqueus' );
 
 
 include_once 'card/index.php';
@@ -54,3 +57,28 @@ function flair_sidler_script() {
 add_action( 'wp_enqueue_scripts', 'flair_sidler_script' );
 
 
+
+
+function fc_pattern() {
+	register_block_pattern(
+		'flair/card-pattern',
+		array(
+			'title' => __( 'Card pattern', 'jape' ),
+			'description' => _x( 'My great description goes here.', 'Block pattern description', 'flair' ),
+			'content'     => '<!-- wp:group {"className":"card"} -->
+<div class="wp-block-group card"><!-- wp:image {"id":75,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://wordpress.test/wp-content/uploads/2022/03/oriole-1024x768.jpg" alt="an oriole" class="wp-image-75"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading -->
+<h2>My header</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>My excerpt text.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->',
+		)
+	);
+}
+add_action( 'init', 'fc_pattern' );
