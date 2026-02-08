fe86971eb7f4790425c60b0bc2c877ded8480776
John Pennypacker <34277245+johnpennypacker@users.noreply.github.com>
2026-02-08 16:16:29 -0500

    stack POC

--- build/blocks/stack/view.asset.php
+++ build/blocks/stack/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '6c201c30d7e5f44956ff');
+<?php return array('dependencies' => array(), 'version' => '4b22a3f7cfa5180a90fb');
--- src/blocks/stack/view.js
+++ src/blocks/stack/view.js
@@ -1,25 +1,55 @@
+
 /**
- * Use this file for JavaScript code that you want to run in the front-end 
- * on posts/pages that contain this block.
- *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
- *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any 
- * JavaScript running in the front-end, then you should delete this file and remove 
- * the `viewScript` property from `block.json`. 
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
+ * 
  */
- 
-/* eslint-disable no-console */
-console.log("Hello World! (from create-block-stack block)");
-/* eslint-enable no-console */
+( function() {
+
+	window.addEventListener("DOMContentLoaded", OKZoomer);
+
+	function OKZoomer() {
+
+		const stacks = document.querySelectorAll(".flair-stack");
+	
+		stacks.forEach(function(el) {
+			el.style.setProperty( "--mouse-pos-x", 0 );
+			el.style.setProperty( "--mouse-pos-y", 0 );
+			el.addEventListener( "mousemove", watchMouse, true );
+		});
+
+	}
+
+	function watchMouse(e) {
+        var el = e.target.closest('.flair-stack');
+		var elRect = e.target.getBoundingClientRect();
+		var x = e.clientX - elRect.left;
+		var y = e.clientY - elRect.top;
+		el.style.setProperty( "--mouse-pos-x", x );
+		el.style.setProperty( "--mouse-pos-y", y );
+
+		var px = x / elRect.width;
+		var py = y / elRect.height;
+		// el.style.setProperty( "--mouse-pct-x", (px*100)+"%" );
+		// el.style.setProperty( "--mouse-pct-y", (py*100)+"%" );
+		el.style.setProperty( "--mouse-pct-x", px );
+		el.style.setProperty( "--mouse-pct-y", py );
+
+		el.style.setProperty( "--mouse-bias-x", calculateBias(px) );
+		el.style.setProperty( "--mouse-bias-y", calculateBias(py) );
+    }
+
+    function calculateBias(v) {
+        if( .5 == v ) {
+            return 0;
+        }
+        if( v > .5 ) {
+            return (v - .5) * 2;
+        }
+        if( v < 50 ) {
+            return (.5 - v) * -2;
+        }
+        return v;
+    }
+
+
+
+})();
