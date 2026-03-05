c45360f16c7875d25067a61f3e413084fbde9c43
John Pennypacker <john@pennypacker.net>
2026-03-05 12:12:42 -0500

    removes placeholders

--- template-parts/multibutton.php
+++ template-parts/multibutton.php
@@ -3,27 +3,10 @@
 	// args isn't always set, if it isn't, use the defaults
 	$attributes = $args['attributes'];
 
-
-// 	return array(
-// 			'labels' => 'First-Year Applicant||Transfer Applicant||Graduate Applicant', // link text
-// 			'links' => 'https://www.clarku.edu/undergraduate-admissions/apply/#app||https://www.clarku.edu/undergraduate-admissions/apply/transfer-students/||https://www.clarku.edu/graduate-education/admissions/',
-// 			'before' => '<div class="clarku-admission-multibutton">',
-// 			'after' => '</div>',
-//     );
-//
-// 	$links = explode( '||', $attributes['links'] );
-// 	$labels = explode( '||', $attributes['labels'] );
-
-
-	$links = explode( '||', 'https://www.clarku.edu/undergraduate-admissions/apply/#app||https://www.clarku.edu/undergraduate-admissions/apply/transfer-students/||https://www.clarku.edu/graduate-education/admissions/' );
-	$labels = explode( '||', 'First-Year Applicant||Transfer Applicant||Graduate Applicant' );
-
-
 	$classes = ['flair-wrapper flair-multibutton'];
 	if( isset( $attributes['orientation'] ) ) {
 		$classes[] = 'orientation-' . $attributes['orientation'] ;
 	}
-//  echo '<pre>', print_r($attributes, TRUE), '</pre>';
 
 ?>
 
@@ -32,7 +15,7 @@
 		<p class="label">I am a:</p>
 
 		<div class="dropdown">
-			<a aria-expanded="false" aria-haspopup="true" class="button dropdown-toggle" href="<?php echo $links[0]; ?>" id="dropdown-menu"><?php echo $labels[0]; ?></a>
+			<a aria-expanded="false" aria-haspopup="true" class="button dropdown-toggle" href="#"> </a>
 			<div class="options" tab-index="0">
 				<?php echo $args['content']; ?>
 			</div>
