b653daf56b1283793273f06e3c265e9c1dced56e
John Pennypacker <john@pennypacker.net>
2026-04-20 18:39:21 -0400

    adjusts empty slide styles

--- build/blocks/carousel-slide/index.asset.php
+++ build/blocks/carousel-slide/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'db794252a003a0f45d2b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '55cf2b548928edf01338');
