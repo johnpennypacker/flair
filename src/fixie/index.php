b61bac2977a7c23d8b248b3b4f036ae0190da094
John Pennypacker <john@pennypacker.net>
2026-05-01 22:35:09 -0400

    successfully loads zoomer and fixie assets conditionally (mostly)

--- src/fixie/index.php
+++ src/fixie/index.php
@@ -7,27 +7,14 @@
 /**
  * Add the fixie script and stylesheet.
  */
-function flair_fixie_enqueues() {
-	// wp_enqueue_script( 'flair-fixie', FLAIR_URL . 'src/fixie/fixie.js', array('wp-hooks'), '', array( 'in_footer' => true, 'strategy'  => 'defer') );
-
-	$asset = include FLAIR_PATH . 'build/fixie/variation.asset.php';
-	wp_enqueue_style( 'flair-fixie', FLAIR_URL . 'build/fixie/frontend.css', [], $asset['version'] );
+function flair_fixie_enqueues( $block_content, $block ) {
+	if ( FALSE !== strpos( $block['attrs']['className'], 'flair-fixie' ) ) {
+		$asset = include FLAIR_PATH . 'build/fixie/variation.asset.php';
+		wp_enqueue_style( 'flair-fixie', FLAIR_URL . 'build/fixie/frontend.css', [], $asset['version'] );
+	}
+	return $block_content;
 }
-add_action( 'wp_enqueue_scripts', 'flair_fixie_enqueues' );
-
-// function flair_fixie_enqueues() {
-//
-// 	$asset = include FLAIR_PATH . 'build/fixie/variation.asset.php';
-// 	wp_enqueue_block_style( 'core/cover', array(
-// 		'handle' => 'flair-fixie',
-// 		'src' => FLAIR_URL . 'build/fixie/frontend.css',
-// 		'path' => FLAIR_PATH . 'build/fixie/frontend.css',
-// 		'deps' => $asset['dependencies'],
-// 		'ver' => $asset['version'],
-// 	));
-// }
-// add_action( 'after_setup_theme', 'flair_fixie_enqueues' );
-
+add_action( 'render_block_core/cover', 'flair_fixie_enqueues', 10, 2 );
 
 /**
  * Enqueue editor assets.
